070191b591b3b1e89af2a8001dad2e4de06389b9
nicholas-long <nicholas.eden.long@gmail.com>
2021-02-05 12:46:51 -0600

    initial commit of code

---
+++ test.py
@@ -0,0 +1,41 @@
+import asyncio
+from aiohttp import ClientSession
+import pymyq
+import json
+import datetime
+import dateutil.parser
+import configparser
+
+config = configparser.ConfigParser()
+config.read('settings.conf')
+
+async def handle_garage(device):
+    max_minutes = int(config['Duration']['max_minutes_open'])
+    if (device.state == "open"):
+      updateTime = device.device_json['state']['last_update']
+      time = dateutil.parser.parse(updateTime)
+      now = datetime.datetime.now(datetime.timezone.utc)
+      difference = now - time
+      print("Open for")
+      print(difference)
+      maxTime = datetime.timedelta(minutes=max_minutes)
+      if (difference > maxTime):
+        await device.close()
+
+
+async def main() -> None:
+    """Create the aiohttp session and run."""
+    async with ClientSession() as websession:
+      email = config['Login']['email']
+      password = config['Login']['password']
+      myq = await pymyq.login(email, password, websession)
+      # Return *all* devices:
+      devices = myq.devices
+      for key in devices:
+          device = devices[key]
+          if (device.device_family == 'garagedoor' and device.close_allowed):
+              await handle_garage(device)
+
+
+
+asyncio.get_event_loop().run_until_complete(main())
